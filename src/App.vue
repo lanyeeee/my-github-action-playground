fc62881d0b8c2079a4eb88a14aaaab48c0cb40db
lanyeeee <1210347077@qq.com>
2024-09-06 09:17:12 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+import AppContent from "./AppContent.vue";
+</script>
+
+<template>
+  <n-modal-provider>
+    <n-notification-provider placement="bottom-right">
+      <n-message-provider>
+        <app-content/>
+      </n-message-provider>
+    </n-notification-provider>
+  </n-modal-provider>
+</template>
